64431c5b3fc64de1d1ea9086a1a0de6f4d387ecf
Igor Kim <igor.kim.dev@pm.me>
2026-07-01 12:19:59 +0200

    Implement main function to process audio normalization and ONNX bioacoustic predictions

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import logging
 
+from chirps.audio.normalize import NormalizeAudio
+from chirps.ml.onnx_bioacoustics import ONNXBioacousticsPredictor
+
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def main():
-    pass
+    file_path = 'demo/2034488.wav'
+
+    normalizer = NormalizeAudio()
+    normalizer.configure({"normalize_level": -3.0})
+
+    result = normalizer.process(path=file_path)
+
+    print(result)
+
+    onnx_bioacoustics = ONNXBioacousticsPredictor()
+    onnx_bioacoustics.configure({"model_path": "chirps/ml/models/onnx_bioacoustics.onnx"})
+    prediction = onnx_bioacoustics.process(path=result['output_path'])
+
+    print(prediction)
+
 
 if __name__ == "__main__":
     main()
