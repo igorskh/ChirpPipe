088e1dd4f78e2edcf7a6e248b3ce5c1e2392b3a2
Igor Kim <igor.kim.dev@pm.me>
2026-07-23 13:33:26 +0200

    enhance ExifTool to support GPS coordinate setting and retrieval, and improve metadata handling

--- chirps/image/exiftool.py
+++ chirps/image/exiftool.py
@@ -16,8 +16,16 @@ class ExifTool(CLIChirp, ChirpNode):
         parser.add_argument(
             "--set_rating", type=int, default=None,
             help="Set the rating for the image (0-5). 0 removes the rating.")
+        parser.add_argument(
+            "--set_gps", type=str, default=None,
+            help="Set the GPS coordinates for the image in the format 'latitude,longitude'.")
         parser.add_argument("--get_rating", action="store_true",
                             help="Get the rating for the image.")
+        parser.add_argument("--get_metadata", action="store_true",
+                            help="Get the metadata for the image.")
+        parser.add_argument(
+            "--get_gps", action="store_true",
+            help="Get the GPS coordinates for the image.")
         return parser.parse_args()
 
     def process_cli(self, args: argparse.Namespace) -> None:
@@ -25,11 +33,18 @@ class ExifTool(CLIChirp, ChirpNode):
         res = self.process(
             path=args.path,
             set_rating=args.set_rating,
-            get_rating=args.get_rating
+            get_rating=args.get_rating,
+            set_gps=args.set_gps,
+            get_gps=args.get_gps,
+            get_metadata=args.get_metadata,
         )
         logging.info(f"ExifTool processing completed for {args.path}.")
         if args.get_rating:
             logging.info(f"Rating: {res['rating']}")
+        if args.get_gps:
+            logging.info(f"GPS: {res['gps']}")
+        if args.get_metadata:
+            logging.info(f"Metadata: {res['metadata']}")
 
     def configure(self, input_config: dict):
         pass
@@ -37,7 +52,11 @@ class ExifTool(CLIChirp, ChirpNode):
     def process(self, **kwargs) -> dict:
         set_rating = kwargs.get("set_rating")
         get_rating = kwargs.get("get_rating")
+        set_gps = kwargs.get("set_gps")
+        get_gps = kwargs.get("get_gps")
         path = kwargs.get("path")
+        get_metadata = kwargs.get("get_metadata")
+
         if path is None:
             raise ValueError(
                 "Missing 'path' argument for ExifTool processing.")
@@ -50,13 +69,41 @@ class ExifTool(CLIChirp, ChirpNode):
 
                 rating_arg = "-Rating=" if set_rating == 0 else f"-Rating={set_rating}"
                 et.execute(rating_arg, path)
+
+            if set_gps is not None:
+                if set_gps == "":
+                    et.execute("-GPSLatitude=", "-GPSLongitude=", path)
+                    return {"metadata": et.get_metadata(path), "rating": None, "gps": None}
+
+                try:
+                    latitude, longitude = map(float, set_gps.split(","))
+                except ValueError:
+                    raise ValueError(
+                        "GPS coordinates must be in the format 'latitude,longitude'.")
+                et.execute(f"-GPSLatitude={latitude}",
+                           f"-GPSLongitude={longitude}", path)
+
             metadata = et.get_metadata(path)
             if get_rating:
                 rating = metadata[0].get("XMP:Rating")
             else:
                 rating = None
 
-        return {"metadata": metadata, "rating": rating}
+            if get_gps:
+                gps = metadata[0].get("EXIF:GPSLatitude"), metadata[0].get(
+                    "EXIF:GPSLongitude")
+            else:
+                gps = None
+
+        result = {}
+        if get_metadata:
+            result["metadata"] = metadata[0]
+        if get_rating:
+            result["rating"] = rating
+        if get_gps:
+            result["gps"] = gps
+
+        return result
 
 
 if __name__ == "__main__":
